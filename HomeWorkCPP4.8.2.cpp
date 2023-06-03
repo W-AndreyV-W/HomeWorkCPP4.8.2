94bb0151b5c0a7855dd4e1a39841db91525695b9
Andrey V <Andreyxvv@hotmail.com>
2023-06-03 13:54:47 +0500

    second commit

--- HomeWorkCPP4.8.2.cpp
+++ HomeWorkCPP4.8.2.cpp
@@ -6,7 +6,7 @@ class node
 {
 public:
     int m_value;
-    std::shared_ptr<node> parent;
+    std::weak_ptr<node> parent;
     node(int value) : m_value{ value } {};
     ~node() { std::cout << "destructor called\n"; }
 };
@@ -15,6 +15,7 @@ int main()
 {
     auto node1 = std::make_shared<node>(1);
     auto node2 = std::make_shared<node>(2);
+    node2->parent = node1;
     node2->parent = node1;
 
     return 0;
